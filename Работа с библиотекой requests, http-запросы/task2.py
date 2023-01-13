a317a15452f5385e41c0c48daf0cb81a1759466c
Retroman95 <46816641+Retroman95@users.noreply.github.com>
2023-01-13 21:53:10 +0400

    Update task2.py

--- Работа с библиотекой requests, http-запросы/task2.py
+++ Работа с библиотекой requests, http-запросы/task2.py
@@ -1,31 +1,32 @@
+import os
 import requests
-from pprint import pprint
-
-HADERS = {'Authorization': 'OAuth твой_скопированный токен' } # В Яндексе необходимо указывать токен в формате 'Authorization': 'OAuth твой_скопированный токен'
 
 class YaUploader:
-    def __init__(self, token: str):
-        self.token = token
-        resp1 = requests.get(
-            'https://cloud-api.yandex.net/v1/disk/resources/upload',
-            params={'path': '/avesome2.gif', 'overwrite': 'true'},
-            headers=token
-        )
-        resp1.raise_for_status()
-        d = resp1.json()
-        global href
-        href = d['href']
+	def __init__(self, token: str):
+		self.token = token
 
-    def upload(self, file_path: str):
-        """Метод загруджает файл file_path на яндекс диск"""
-        # Тут ваша логика
+	def get_headers(self):
+		return {
+			'Content-Type': 'application/json',
+			'Authorization': 'OAuth {}'.format(self.token)
+		}
 
-        with open(file_path, 'rb') as f:
-            resp2 = requests.put(href, files={'file': f})
+	def _get_upload_link(self, file_name):
+		upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+		params = {'path': file_name, 'overwrite': 'true'}
+		response = requests.get(upload_url, headers=self.get_headers(), params=params)
+		return response.json()['href']
 
-        return 'Вернуть ответ об успешной загрузке'
-
+	def upload(self, file_path: str):
+		"""Метод загружает файлы по списку file_list на яндекс диск"""
+		# Тут ваша логика
+		# Функция может ничего не возвращать
+		upload_url = self._get_upload_link(os.path.basename(file_path))
+		response = requests.put(upload_url, data=open(path_to_file, 'rb'), headers=self.get_headers())
 
 if __name__ == '__main__':
-    uploader = YaUploader(HADERS)
-    result = uploader.upload('../../dz1_Python/lecture9_gifs/Obey the Flame.gif')
+	# Получить путь к загружаемому файлу и токен от пользователя
+	path_to_file = input('Введите путь к файлу: ') 
+	token = input('Введите токен: ') # Здесь вводится полученный на полигоне токен (токен можно получить кликнув на полигоне на кнопку "Получить OAuth-токен")
+	ya = YaUploader(token)
+	ya.upload(path_to_file)
