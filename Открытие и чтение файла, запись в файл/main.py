d8ee523a006e63b64fe3025694dad038c2bfdcfe
Retroman95 <46816641+Retroman95@users.noreply.github.com>
2023-01-14 00:22:35 +0400

    Update main.py

--- Открытие и чтение файла, запись в файл/main.py
+++ Открытие и чтение файла, запись в файл/main.py
@@ -3,44 +3,7 @@ import time
 from pprint import pprint
 
 
-cook_book = {
-  'Омлет': [
-    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
-    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
-    ],
-  'Утка по-пекински': [
-    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
-    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
-    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
-    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
-    ],
-  'Запеченный картофель': [
-    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
-    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
-    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
-    ]
-  }
-
 def read_cookbook():
-    file_path = os.path.join(os.getcwd(), 'recipes.txt')
-    cook_book = {}
-    with open(file_path, 'r', encoding='utf-8') as f:
-        for line in f:
-            dish_name = line.strip()
-            count = int(f.readline())
-            ing_list = list()
-            for item in range(count):
-                ingrs = {}
-                ingr = f.readline().strip()
-                ingrs['ingredient_name'], ingrs['quantity'], ingrs['measure'] = ingr.split('|')
-                ingrs['quantity'] = int(ingrs['quantity'])
-                ing_list.append(ingrs)
-            f.readline()
-            cook_book[dish_name] = ing_list
-    return cook_book
-
-def read_cookbook2():
     cook_book = {}
     with open('recipes.txt', encoding='utf-8') as f:
         for line in f:
@@ -60,12 +23,12 @@ def get_shop_list_by_dishes(dishes, person_count):
         if dish_name in cook_book:
             for ings in cook_book[dish_name]:  # итерируем ингридиенты в блюде
                 meas_quan_list = dict()
-                if ings['ingredient_name'] not in ingr_list:
-                    meas_quan_list['measure'] = ings['measure']
+                if ings['ingredient_name']not in ingr_list:
+                    meas_quan_list['measure'] = ings['measure'].strip()
                     meas_quan_list['quantity'] = ings['quantity'] * person_count
-                    ingr_list[ings['ingredient_name']] = meas_quan_list
+                    ingr_list[ings['ingredient_name'].strip()] = meas_quan_list
                 else:
-                    ingr_list[ings['ingredient_name']]['quantity'] = ingr_list[ings['ingredient_name']]['quantity'] + \
+                    ingr_list[ings['ingredient_name'].strip()]['quantity'] = ingr_list[ings['ingredient_name'].strip()]['quantity'] + \
                                                                      ings['quantity'] * person_count
 
         else:
@@ -145,10 +108,8 @@ def rewrite_file(path1=None, path2=None, path3=None):
 if __name__ == '__main__':
     filename = "recipes.txt"
     cook_book = read_cookbook()
-    cook_book2 = read_cookbook2()
     print('Задание 1------------------------------------------------------------')
     print(f'cook_book = {cook_book}')
-    print(f'cook_book = {cook_book2}')
     print('Задание 2------------------------------------------------------------')
     pprint(get_shop_list_by_dishes(dishes=['Запеченный картофель', 'Омлет'], person_count=2))
     print('Задание 3------------------------------------------------------------')
